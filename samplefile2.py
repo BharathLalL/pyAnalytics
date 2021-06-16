550cde9bd6d531574531ed89d75a07c20dcc83d6
Bharath Lal <h20200825@pilani.bits-pilani.ac.in>
2021-06-16 17:35:10 +0530

    Bharath

---
+++ samplefile2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:32:58 2021
+
+@author: user1
+"""
+
+#this file has been created by Bharath Lal for the purpose of learning how to create a file and commit it to main and upload to git account
